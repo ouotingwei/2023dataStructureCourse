252ea152649c119dbc30c336580eb17ebf0f6ba2
tingweiouo <kklb1716@gmail.com>
2023-01-28 22:52:07 +0800

    queue_array

---
+++ queue_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"dataStructure.h"
+
+#define MAX_ITEM 7
+
+using namespace std;
+
+typedef struct tagQueue{
+    int item[MAX_ITEM];
+    int front = -1;
+    int rear = -1;
+
+}Queue;
+
+int main(){
+    Queue Que;
+
+    return 0;
+}
