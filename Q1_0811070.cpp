d9ca504b703665be09b4e9c6f20444ef5b565a1d
tingweiouo <kklb1716@gmail.com>
2023-03-02 21:26:20 +0800

    hw1 done

--- Q1_0811070.cpp
+++ Q1_0811070.cpp
@@ -2,75 +2,41 @@
 
 using namespace std;
 
-int inputSize();
+int n;
+int inputN();
 int inputLeaves();
-int FindMaxJump(int[], int n);
+int FindMaxJump(int[]);
 
 int main(int argc, char **argv){
-    int maxJump = 0;
-    int row = inputSize();
-    int column = inputSize();
-    int leaves2d[row][column] = {0};
-
-    for( int i = 0; i < row; i++){
-        for( int j = 0; j < column; j++){
-            //cout << i << " " << j << " :" ;
-            leaves2d[i][j] = inputLeaves();
-        }
+    n = inputN();
+    int leaves[n] = {0};
+    for(int i = 0; i < n; i++){
+        leaves[i] = inputLeaves();
     }
 
-    /*
-    for(int i = 0; i < row; i++){
-        int leaves[column] = {0};
-        int tempMaxJump = 0;
-
-        for(int j = 0; j < column; j++){
-            leaves[j] = leaves2d[i][j];
-        }
-
-        tempMaxJump = FindMaxJump(&leaves[0], column);
-        if(tempMaxJump > maxJump){
-            maxJump = tempMaxJump;
-        }
-
-    }
-    */
-
-    for(int i = 0; i < column; i++){
-        int leaves[row] = {0};
-        int tempMaxJump = 0;
-
-        for(int j = 0; j < row; j++){
-            leaves[j] = leaves2d[j][i];
-        }
-
-        tempMaxJump = FindMaxJump(&leaves[0], row);
-        if(tempMaxJump > maxJump){
-            maxJump = tempMaxJump;
-        }
-    }
-
-    //cout << maxJump << endl;
-
+    cout << FindMaxJump(&leaves[0]) << endl;
     return 0;
 }
 
-int inputSize(){
+//scan the size of array
+int inputN(){
     int input = 0;
     cin >> input;
-    if ( input >= 1 && input <= 1000 ){
+    if ( input >= 1 && input <= 1000){
         return input;
     }else{
         cout << "[W]input error !" << endl;
-        return inputSize();
+        return inputN();
     }
+
 }
 
+//scan the input element of the array
 int inputLeaves(){
-    int input = 0;
-    cin >> input;
-    if( input >= -1 && input <= 1 ){
-        return input;
+    int leavesInput = 0;
+    cin >> leavesInput;
+    if ( leavesInput >= -1 && leavesInput <= 1 ){
+        return leavesInput;
     }else{
         cout << "[W]input error !" << endl;
         return inputLeaves();
@@ -78,11 +44,11 @@ int inputLeaves(){
 
 }
 
-int FindMaxJump(int *leaves, int n){
+//find max jump
+int FindMaxJump(int *leaves){
     int maleCNT = 0;
     int cnt = 0;
     int maxjump = 0;
-
     for(int i=0; i<n; i++){
         if(*(leaves + i) == 1){
             maleCNT ++ ;
@@ -105,7 +71,7 @@ int FindMaxJump(int *leaves, int n){
         bool flag = true;
         cnt = start;
         while(flag == true){
-            if(cnt == n - 1){
+            if(cnt == n - 1 && *(leaves + cnt) != -1){
                 flag = false;
                 //cout << "touch1" << endl;
             }else if(*(leaves + cnt + 1) == 1){
@@ -136,7 +102,7 @@ int FindMaxJump(int *leaves, int n){
         bool flag = true;
         cnt = start;
         while(flag == true){
-            if(cnt == 0){
+            if(cnt == 0 && *(leaves + i) != -1){
                 flag = false;
                 //cout << "touch1" << endl;
             }else if(*(leaves + cnt - 1) == 1){
@@ -155,13 +121,11 @@ int FindMaxJump(int *leaves, int n){
         flag = true;
         if(jump > maxjump){
             maxjump = jump;
+            //cout <<"maxjump = "<<maxjump << endl;
         }
         
     }
-    cout << " "<< endl;
-    cout << maxjump << endl;
-    cout << " "<< endl;
+
     return maxjump;
 }
-
 
