597df882d9fe3009f02470d1eab98a69cd0c605a
tingweiouo <kklb1716@gmail.com>
2023-03-06 22:20:30 +0800

    hw2 debug not yet

--- hw2/Q1_0811070.cpp
+++ hw2/Q1_0811070.cpp
@@ -76,47 +76,54 @@ int expCompare(int expA, int expB) {
 
 void addPolynomial(int m, int n){
     int startA = 0;
-    int endA = m - 1;
+    int endA = m ;
     int startB = m;
-    int endB = m + n - 1;
-    avail = m + n;
+    int endB = m + n ;
+    avail = m + n ;
     int i = 0;
 
+    cout << "endA = " << endA << " endB = " << endB << endl;
+
     while( startA <= endA && startB <= endB ){
-        switch( expCompare(terms[startA].expon, terms[startB].expon) ){
+        int com = expCompare(terms[startA].expon, terms[startB].expon);
+        cout << "startA = " <<startA << " startB = " << startB << " avail = " << avail << endl;
+
+        switch( com ){
             case -1: // expA < expB
                 terms[avail].coef = terms[startB].coef;
                 terms[avail].expon = terms[startB].expon;
-                
+                cout << "case -1" << endl;
                 startB ++;
-                avail++;
 
                 break;
             case 0: // expA = expB
-                terms[avail].coef = terms[startA].coef + terms[startB].coef;
+                terms[avail].coef = ( terms[startA].coef + terms[startB].coef );
                 terms[avail].expon = terms[startB].expon;
+                
 
                 startA ++;
                 startB ++;
-                avail++;
+                cout << "case 0" << endl;
 
                 break;
             case 1: // expA > expB
                 terms[avail].coef = terms[startA].coef;
                 terms[avail].expon = terms[startA].expon;
+                cout << "case 1" << endl;
 
                 startA ++;
-                avail++;
 
                 break;
         }
+
+        avail ++;
 
     }
 
     cout << endl;
 
 
-    for(int i = 0; i < avail; i++){
+    for(int i = (m + n); i < avail; i++){
         cout << terms[i].coef << " " << terms[i].expon << endl;
     }
 
