067d5eae894af63324b550c29b1504789e2da045
TingWei Ou <87459096+ouotingwei@users.noreply.github.com>
2023-03-20 16:45:41 +0800

    Update Q1_0811070.cpp

--- hw3/Q1_0811070.cpp
+++ hw3/Q1_0811070.cpp
@@ -1,4 +1,4 @@
-/*Data Structure HW3-1 by TingweiOu 0811070*/
+/*Data Structure HW3 by TingweiOu 0811070*/
 #include<iostream>
 #define MAX_ITEM 100
 
@@ -19,54 +19,39 @@ int inputCheckPoints();
 int main(){
 	stack stk;
 	int N = inputCheckN();
-	int outPut[N] = {0};
-	bool flag = true;
+	int Points[N] = {0};
+	int span[N] = {0};
 
 	//scan input data
 	
 	for(int i = 0; i < N; i++){
-		outPut[i] = inputCheckPoints();
+		Points[i] = inputCheckPoints();
 	}	
-
-	int CNT = 0;
-	while(flag == true){
-		
-		cout << endl;
-		cout << "cnt : " << CNT << endl;
-		cout << "outPut[CNT] : " << outPut[CNT] << endl;
-		cout << "stk.Top : " << stk.Top << endl;
-		cout << "outPut[CNT - 1]" << outPut[CNT - 1] << endl;
-		
-		if(isEmpty(&stk) == false && stk.Item[stk.Top] <= outPut[CNT]){
-			cout << "[case 1]" << endl;
-			//cout << stk.Item[stk.Top] << endl;
-			pop(&stk); // !
-		}else if(isEmpty(&stk) == true ){
-			cout << "[case 2]" << endl;
-			push(&stk, outPut[CNT]);
-			cout << "stk.Item[stk.Top] : " << stk.Item[stk.Top] << endl;
-			outPut[CNT] = CNT - stk.Top + 1; 
-			CNT++;
-		}else{
-			cout << "[case 3]" << endl;
-			push(&stk, outPut[CNT]);
-			cout << "stk.Item[stk.Top] : " << stk.Item[stk.Top] << endl;
-			outPut[CNT] = CNT - stk.Top + 1; 
-			CNT ++;
-		}
-		
-		if(CNT == N){
-			flag = false;
-		}
 	
+	for(int i = 0; i < N; i++ ){
+		while(1){
+			if(isEmpty(&stk) == false){
+				if(Points[  stk.Item[stk.Top] ] <= Points[i]){
+					pop(&stk);
+				}else{
+					span[i] = i - stk.Item[stk.Top];
+					push(&stk, i);
+					
+					break;
+				}
+				
+			}else{
+				span[i] = i + 1;
+				push(&stk, i);
+				
+				break;
+			}
+		}
 	}
-
-	cout << endl;
 	
 	for(int i = 0; i < N; i++){
-		cout << outPut[i] << " ";
+		cout << span[i] << " ";
 	}
-
 	cout << endl;
 	
 	return 0;
