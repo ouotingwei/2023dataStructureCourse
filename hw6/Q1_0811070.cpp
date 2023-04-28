69f2b327dc1243942a1e77b781edd3e4dd948626
tingweiouo <kklb1716@gmail.com>
2023-04-28 15:33:43 +0800

    hw6 not yet

--- hw6/Q1_0811070.cpp
+++ hw6/Q1_0811070.cpp
@@ -16,19 +16,63 @@ void input()
     for(int i = 0; i < amount; i++){
         int data = 0;
         cin >> data;
+        
+        if(data < 0 && data >500){
+            break;
+        }
+
         BinaryTree[i][0] = data;
-        BinaryTree[i][1] = -1;  // if BinaryTree[i][1] == 0 -> have been traversed
+        BinaryTree[i][1] = false;
     }
     
     return;
 }
 
 void Traverse()
+{   
+    int step = 0;
+    int node_locate = 0;
+    int node_value = 0;
+    cin >> node_value >> step;
+
+    for(int i = 0; i < amount; i++){
+        if(BinaryTree[i][0] == node_value){
+            node_locate = i;
+            BinaryTree[i][1] = 0;
+            break;
+        }
+    }
+
+    while(1){
+        
+    }
+    
+    return;
+}
+
+int to_father(int node_locate)
+{   
+    if(node_locate % 2 == 0){    //even
+        node_locate = (node_locate - 2) / 2;
+    }else{  //odd
+        node_locate = (node_locate - 1) / 2;
+    }
+
+    BinaryTree[node_locate][1] = 0;
+
+    return node_locate;
+}
+
+int to_left_child(int node_locate)
 {
-    int value, step, step_cnt = 0;
-    cin >> value >> step;
-    
-    
+    node_locate = (node_locate * 2) + 1;
+    return node_locate;
+}
+
+int to_right_child(int node_locate)
+{
+    node_locate = (node_locate * 2) + 2;
+    return node_locate;
 }
 
 int main(){
