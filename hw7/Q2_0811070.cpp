b60ab2aa3f111776303f7407055fd03a7b70b871
tingweiou <kklb1716@gmail.com>
2023-05-25 23:51:59 +0800

    hw7-2 not yet

--- hw7/Q2_0811070.cpp
+++ hw7/Q2_0811070.cpp
@@ -1,73 +1,41 @@
 #include<iostream>
-#include<queue>
+#include<algorithm>
+#include<vector>
 
 using namespace std;
 
-void cost_input(int **cost, int S){
-    for(int i = 0; i < S; i++){
-        int a, b, d;
-        cin >> a >> b >> d;
-        cost[a-1][b-1] = d;
-        cost[b-1][a-1] = d;
-    }
+typedef struct{
+    int noise;
+    int edge1;
+    int edge2;
+}street;
+
+bool compare(street a, street b){
+    return a.noise < b.noise;
 }
 
-void find_minimum_BFS(int **cost, int start, int end, int S){
-    int smallest = 20001;
-    bool *visited = new bool[S];
-    for(int i = 0; i < S; i++){
-        visited[i] = false;
+vector<street> input_edge(int s){
+    vector<street> p;
+    int a, b, cost;
+    street input;
+    for(int i=0; i<s; i++){
+        cin>>a>>b>>cost;
+        input.edge1 = a;
+        input.edge2 = b;
+        input.noise = cost;
+        p.push_back(input);
     }
 
-    queue<int> q;
-
-    visited[start] = true;
-    q.push(start);
-
-    while(!q.empty()){
-        int current = q.front();
-        q.pop();
-
-        if(current == end){
-            break;
-        }
-
-        for(int neighbor = 0; neighbor < S; neighbor++){
-            if(cost[current][neighbor] != 0 && cost[current][neighbor] != 20001 && visited[neighbor] == false){
-                visited[neighbor] = true;
-                if(cost[current][neighbor] < smallest){
-                    smallest = cost[current][neighbor];
-                }
-            
-            q.push(neighbor);
-            }
-        }
-    }
-    cout << smallest << endl;
+    sort(p.begin(), p.end(), compare);
+    return p;
 }
+
+
 
 int main(){
     int C, S, Q;
     cin >> C >> S >> Q;
-
-    int **cost = new int*[C];
-    for(int i = 0; i < C; i++){
-        cost[i] = new int[C];
-    }
-
-    for(int i = 0; i < C; i++){
-        for(int j = 0 ; j < C; j++){
-            if(i == j){
-                cost[i][j] = 0;
-            }else{
-                cost[i][j] = 20001;
-            }  
-        }
-    }
-
-    cost_input(cost, S);
-    find_minimum_BFS(cost, 1, 7, S);
-
-
+    vector<street> path = input_edge(S);
+    
     return 0;
 }
