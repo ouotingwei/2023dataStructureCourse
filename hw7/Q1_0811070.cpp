35d1071c8fdeecc7da4a8f2f7c7ee443c7bebf19
TingWei Ou <87459096+ouotingwei@users.noreply.github.com>
2023-05-31 21:18:08 +0800

    Update Q1_0811070.cpp

--- hw7/Q1_0811070.cpp
+++ hw7/Q1_0811070.cpp
@@ -2,80 +2,75 @@
 
 using namespace std;
 
-void cost_input(int **cost, int S){
-    for(int i = 0; i < S; i++){
-        int a, b, d;
-        cin >> a >> b >> d;
-        cost[a-1][b-1] = d;
-        cost[b-1][a-1] = d;
-    }
-}
-
-void allcost(int **cost, int **distance, int n){
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            distance[i][j] = cost[i][j];
-        }
-    }
-
-    for(int k = 0; k < n; k++){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(distance[i][k] + distance[k][j] < distance[i][j]){
-                    distance[i][j] = distance[i][k] + distance[k][j];
-                } 
-            }
-        }
-    } 
-}
-
-void output(int **distance, int Q){
-    int *outa = new int[Q];
-    int *outb = new int[Q];
-    for(int i = 0; i < Q; i++){
-        int a, b;
-        cin >> a >> b;
-        outa[i] = a-1;
-        outb[i] = b-1;
-    }
-
-    for(int i = 0; i < Q; i++){
-        if(distance[outa[i]][outb[i]] == 20001){
-            cout << "no path" << endl;
-        }else{
-            cout << distance[outa[i]][outb[i]] << endl;
-        }
-    }
-
-}
+int C, S, Q;
+void input_cost(int **);
+void allcost(int **);
+void output(int **);
 
 int main(){
-    int C, S, Q;
-    cin >> C >> S >> Q;
+	cin >> C >> S >> Q;
+	if(C<2 && C>100 && S<1 &&S>1000 && Q<1 && Q>1000){
+		return -1;
+	}
+	
+	int **cost = new int*[C];
+	for(int i = 0; i < C; i++){
+		cost[i] = new int[C];
+	} 
+	
+	input_cost(cost);
+	allcost(cost);
+	output(cost);
+	
+	return 0;
+}
 
-    int **cost = new int*[C];
-    int **distance = new int*[C];
-    for(int i = 0; i < C; i++){
-        cost[i] = new int[C];
-        distance[i] = new int[C];
-    }
+void input_cost(int **cost){
+	for(int i=0; i<C; i++){
+		for(int j=0; j<C; j++){
+			if(i == j){
+				cost[i][j] = 0;
+			}else{
+				cost[i][j] = 20001;
+			}
+		}
+	}
+	
+	int a, b, noise = 0;
+	for(int i=0; i<S; i++){
+		cin >> a >> b >> noise;
+		cost[a-1][b-1] = noise;
+		cost[b-1][a-1] = noise;
+	}
+}
 
-    for(int i = 0; i < C; i++){
-        for(int j = 0 ; j < C; j++){
-            if(i == j){
-                cost[i][j] = 0;
-                distance[i][j] = 0;
-            }else{
-                cost[i][j] = 20001;
-                distance[i][j] = 20001;
-            }  
-        }
-    }
+void allcost(int **cost){
+	for(int k=0; k<C; k++){
+		for(int i=0; i<C; i++){
+			for(int j=0; j<C;j++){
+				if(cost[i][k] + cost[k][j] < cost[i][j]){
+					cost[i][j] = cost[i][k] + cost[k][j];
+				}
+			}
+		}
+	}
+}
 
-    cost_input(cost, S);
-    allcost(cost, distance, C);
-    output(distance, Q);
-
-    return 0;
+void output(int **cost){
+	int *outa = new int[Q];
+	int *outb = new int[Q];
+	for(int i=0; i<Q; i++){
+		int a, b;
+		cin >> a >> b;
+		outa[i] = a-1;
+		outb[i] = b-1;
+	}
+	
+	for(int i=0; i<Q; i++){
+		if(cost[outa[i]][outb[i]] == 20001){
+			cout << "no path" << endl;
+		}else{
+			cout << cost[outa[i]][outb[i]] << endl;
+		}
+	}
 }
