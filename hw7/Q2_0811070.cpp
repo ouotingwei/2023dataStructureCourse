a30d5dfda9ccdedf83f4086984fb53bb70b5b254
TingWei Ou <87459096+ouotingwei@users.noreply.github.com>
2023-06-01 02:07:22 +0800

    Update Q2_0811070.cpp

--- hw7/Q2_0811070.cpp
+++ hw7/Q2_0811070.cpp
@@ -5,96 +5,96 @@
 using namespace std;
 
 typedef struct{
-    int noise;
-    int edge1;
-    int edge2;
+	int noise;
+	int edge1;
+	int edge2;
 }street;
 
-bool compare(street a, street b){
-    return a.noise < b.noise;
-}
-
-vector<street> input_edge(int S){
-    vector<street> p;
-    int a, b, cost;
-    
-    for(int i=0; i<S; i++){
-        cin >> a >> b >> cost;
-        street input;
-        input.edge1 = a;
-        input.edge2 = b;
-        input.noise = cost;
-        p.push_back(input);
-    }
-
-    sort(p.begin(), p.end(), compare);
-    return p;
-}
-
-int find_root(int *father, int p){
-    if(father[p] != p){
-        int root = find_root(father, father[p]);
-        father[p] = root;
-        return root;
-    }else{
-        return p;
-    }
-}
-
-void union_set(int *father, int a, int b){
-    int root_a = find_root(father, a);
-    int root_b = find_root(father, b);
-    
-    if(root_a != root_b){
-        father[root_a] = root_b;
-    }
-}
-
-int find_minimum_noise(vector<street> st, int C, int S, int a, int b){
-    int rootTree[C];
-    int ed1, ed2, minNoice = -1;
-
-    for(int i = 0; i < C; i++){
-        rootTree[i] = i;
-    }
-
-    for(int i=0; i<S; i++){
-        ed1 = st[i].edge1 - 1;
-        ed2 = st[i].edge2 - 1;
-        union_set(rootTree, ed1, ed2);
-
-        if(find_root(rootTree, a-1) == find_root(rootTree, b-1)){
-            minNoice = st[i].noise;
-            break;
-        }
-    }
-
-    return minNoice;
-}
+int C, S, Q;
+vector<street> input();
+int find_minimum(vector<street>, int a, int b);
+bool compare(street, street);
+void unionset(int *, int, int);
+int find_root(int *, int);
 
 int main(){
-    int C, S, Q, a, b;
-    cin >> C >> S >> Q;
-    int ANS[Q];
+	cin >> C >> S >> Q;
+	vector<street> st = input();
+	int ans[Q];
+	for(int i = 0; i < Q; i++){
+		int a, b;
+		cin >> a >> b;
+		ans[i] = find_minimum(st, a, b);
+	}
+	
+	for(int i = 0; i < Q; i++){
+		if(ans[i] == -1){
+			cout << "no path" << endl;
+		}else{
+			cout << ans[i] << endl;
+		}
+	}
+	
+	return 0;
+}
 
-    for(int i=0; i<Q; i++){
-        ANS[i] = -1;
-    }
+bool compare(street a, street b){
+	return a.noise < b.noise;
+}
 
-    vector<street> graph = input_edge(S);
+vector<street> input(){
+	vector<street> p;
+	int a, b, noise;
+	for(int i = 0; i < S; i++){
+		cin >> a >> b >> noise;
+		street input;
+		input.edge1 = a;
+		input.edge2 = b;
+		input.noise = noise;
+		p.push_back(input);
+	}
+	
+	sort(p.begin(), p.end(), compare);
+	return p;
+}
 
-    for(int i=0; i<Q; i++){
-        cin >> a >> b;
-        ANS[i] = find_minimum_noise(graph, C, S, a, b);
-    }
+int find_root(int *rootTree, int p){
+	if(rootTree[p] != p){
+		int root = find_root(rootTree, rootTree[p]);
+		rootTree[p] = root;
+		return root;
+	}else{
+		return p;
+	}
+}
 
-    for(int i=0; i<Q; i++){
-        if(ANS[i] == -1){
-            cout << "no path" << endl;
-        }else{
-            cout << ANS[i] << endl;
-        }
-    }
-    
-    return 0;
+void unionset(int *rootTree, int ed1, int ed2){
+	int roota = find_root(rootTree, ed1);
+	int rootb = find_root(rootTree, ed2);
+	
+	if(roota != rootb){
+		rootTree[roota] = rootb;
+	}
+}
+
+int find_minimum(vector<street> st, int a, int b){
+	int rootTree[C];
+	for(int i = 0; i < C; i++){
+		rootTree[i] = i;
+	}
+	
+	int ed1, ed2, min = -1;
+	for(int i = 0; i < S; i++){
+		ed1 = st[i].edge1-1;
+		ed2 = st[i].edge2-1;
+		
+		unionset(rootTree, ed1, ed2);
+		
+		if(find_root(rootTree, a-1) == find_root(rootTree, b-1)){
+			min = st[i].noise;
+			break;
+		}
+	}
+	
+	return min;
 }
